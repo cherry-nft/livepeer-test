764ff112bdd3468ea87262b6c0969d6a7a6a8e25
cherry-nft <109643299+cherry-nft@users.noreply.github.com>
2024-03-07 23:38:35 +0000

    livepeer installed api

---
+++ components/livepeer-init.tsx
@@ -0,0 +1,5 @@
+import { Livepeer } from "livepeer";
+
+const livepeer = new Livepeer({
+  apiKey: "49193479-7ddd-4ad3-88df-d37365305859", // Your API key
+});
---
+++ components/source.tsx
@@ -0,0 +1,25 @@
+import { getSrc } from "@livepeer/react/external";
+import * as Player from "@livepeer/react/player";
+
+// An example response from the livepeer playback info API endpoint
+export const vodSource = {
+    type: "vod",
+    meta: {
+        playbackPolicy: null,
+        source: [
+            {
+                hrn: "HLS (TS)",
+                type: "html5/application/vnd.apple.mpegurl",
+                url: "https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/184cfiu4ek66unf4/index.m3u8",
+            },
+        ],
+    },
+};
+
+// This is either an array of `Src` or null,
+// and can be passed into the Player.Root `src`
+const src = getSrc(vodSource);
+
+export default () => (
+    <Player.Root src={src}>{/* All child components. */}</Player.Root>
+);
