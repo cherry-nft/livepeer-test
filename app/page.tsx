fc2ae4ba7346af0c4cbd23cfbcd23060f5ad5b36
cherry-nft <109643299+cherry-nft@users.noreply.github.com>
2024-03-08 00:29:32 +0000

    video portal running

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,57 @@
+"use client";
+
 import DashboardNav from "@/components/dashboard-nav";
 import DashboardTabs from "@/components/dashboard-tabs";
 import DashboardContent from "@/components/dashboard-content";
+import React from "react";
+import { getSrc } from "@livepeer/react/external";
+import * as Player from "@livepeer/react/player";
+import { vodSource } from "@/components/source";
+import { useEffect, useRef, useState } from "react";
 
-export default function DashboardPage() {
-  return (
-    <main>
-      <DashboardNav />
-      <DashboardTabs />
-      <DashboardContent />
-    </main>
-  );
-}
+const DashboardPage = () => {
+  const parentRef = useRef<HTMLDivElement | null>(null);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    if (parentRef.current) {
+      setIsMounted(true);
+    }
+  }, []);
+    return (
+      <main>
+        <DashboardNav />
+        <DashboardTabs />
+         <DashboardContent />
+        <div
+              style={{
+                height: 20,
+                width: "100%",
+              }}
+              ref={parentRef}
+            />
+            <Player.Root src={getSrc(vodSource)} autoPlay volume={0}>
+              <Player.Container
+                style={{
+                  margin: 5,
+                  borderRadius: 5,
+                  outline: "white solid 1px",
+                  overflow: "hidden",
+                }}
+              >
+                <Player.Video
+                  title="Agent 327"
+                  style={{ height: "100%", width: "100%" }}
+                />
+                {isMounted && (
+                  <Player.Portal container={parentRef.current}>
+                    <Player.Time />
+                  </Player.Portal>
+                )}
+              </Player.Container>
+            </Player.Root>
+          </main>
+        );
+      };
+
+      export default DashboardPage;
